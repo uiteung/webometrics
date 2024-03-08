36c85b91cc1bc118c3722637d51fc6102b21f648
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-08 15:49:09 +0700

    create function fetch for calculate publication & citation

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,57 @@
-import { requestOptionsGet, UrlGetRekapCitasiPublikasiPertahun } from "./controller/template.js";
+import { requestOptionsGet, UrlGetRekapCitasiPublikasiPertahun, UrlGetAllPublikasi, UrlGetAllBuku } from "./controller/template.js";
 
+// Fetch Data Get All Publikasi
+fetch(UrlGetAllPublikasi, requestOptionsGet)
+  .then(response => response.json())
+  .then(data => {
+    // Menghitung jumlah total kutipan
+    let totalKutipan = 0;
+    data.data.forEach(publikasi => {
+      totalKutipan += publikasi.jumlah_kutipan;
+    });
+
+    // Menampilkan jumlah total kutipan pada elemen dengan id "jumlahPublikasi"
+    document.getElementById('jumlahPublikasi').textContent = totalKutipan;
+  })
+.catch(error => console.error('Error:', error));
+
+// Fetch Data Get All Buku
+fetch(UrlGetAllBuku, requestOptionsGet)
+  .then(response => response.json())
+  .then(data => {
+    // Menghitung jumlah total kutipan
+    let totalKutipan = 0;
+    data.data.forEach(buku => {
+      totalKutipan += buku.jumlah_kutipan;
+    });
+
+    // Menampilkan jumlah total kutipan pada elemen dengan id "jumlahBuku"
+    document.getElementById('jumlahBuku').textContent = totalKutipan;
+  })
+.catch(error => console.error('Error:', error));
+
+// Fetch Data Get Publikasi Internasional & Nasional
+fetch(UrlGetAllPublikasi, requestOptionsGet)
+  .then(response => response.json())
+  .then(data => {
+    // Menghitung jumlah total data dengan jenis publikasi "Scopus" dan yang bukan "Scopus"
+    let jurnalInterCount = 0;
+    let jurnalNasionalCount = 0;
+    data.data.forEach(publikasi => {
+      if (publikasi.ranking === 'Scopus') {
+        jurnalInterCount++;
+      } else {
+        jurnalNasionalCount++;
+      }
+    });
+
+    // Menampilkan jumlah total data pada elemen dengan id "jurnalInter" dan "jurnalNasional"
+    document.getElementById('jurnalInter').textContent = jurnalInterCount;
+    document.getElementById('jurnalNasional').textContent = jurnalNasionalCount;
+  })
+.catch(error => console.error('Error:', error));
+
+// Untuk Fetch Data in Line Chart
 document.addEventListener("DOMContentLoaded", function() {
     // Fetch data from API
     fetch(UrlGetRekapCitasiPublikasiPertahun, requestOptionsGet)
