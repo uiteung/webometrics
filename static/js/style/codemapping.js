a4f54f32afb5fffb28bbee6a57243bc111e75c28
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-09 17:58:34 +0700

    add option code mapping to name prodi

--- static/js/style/codemapping.js
+++ static/js/style/codemapping.js
@@ -2,6 +2,20 @@
 const codeToNameMapping = {
     "14" : "D4 Teknik Informatika",
     "13" : "D3 Teknik Informatika",
+    "23": "D3 Manajemen Informatika",
+    "33": "D3 Akuntansi",
+    "34": "D4 Akuntansi Keuangan",
+    "43": "D3 Manajemen Pemasaran",
+    "44": "D4 Manajemen Perusahaan",
+    "53": "D3 Administrasi Logistik",
+    "54": "D4 Logistik Bisnis",
+    "74": "D4 Logistik Niaga",
+    "81": "S1 Manajemen Transportasi",
+    "82": "S1 Manajemen Logistik",
+    "83": "S1 Bisnis Digital",
+    "84": "S1 Sains Data",
+    "85": "S1 Manajemen Rekayasa",
+    "101": "S2 Manajemen Logistik"
 };
 
 export let getNameByCode = (code) => codeToNameMapping[code] || 'Tidak Ada';
