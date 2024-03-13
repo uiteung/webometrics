a1d80c17f748c739496ffea6230a1cb25b10f69c
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-13 13:32:51 +0700

    add url new for rekap grafik

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -14,12 +14,16 @@ export let requestOptionsGet = {
 // Endpoint Get All
 export let UrlGetAllPublikasi = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/publikasi";
 export let UrlGetRekapCitasiPublikasiPertahun = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/publikasi/rekapcitasi";
+export let UrlGetRekapCitasiPublikasiPertahunByProdi = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/publikasi/rekapcitasi/get";
 export let UrlGetRekapCitasiPublikasiPertahunByJenis = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/publikasi/citasijenistahun";
+export let UrlGetRekapCitasiPublikasiPertahunByJenisByProdi = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/publikasi/citasijenistahun/get";
 export let UrlGetAllPublikasiByKodeProdi = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/publikasi/get";
 export let UrlGetCitasiPerprodi = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/publikasi/citasiprodi";
 
 export let UrlGetAllBuku = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/buku";
 export let UrlGetRekapCitasiBukuPertahunByJenis = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/buku/citasijenistahun";
+export let UrlGetRekapCitasiBukuPertahunByJenisByProdi = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/buku/citasijenistahun/get";
 export let UrlGetAllBukuByKodeProdi = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/buku/get";
 export let UrlGetRekapCitasiBukuPertahun = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/buku/rekapcitasi";
+export let UrlGetRekapCitasiBukuPertahunByProdi = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/buku/rekapcitasi/get";
 export let UrlGeCitasiBukuPerprodi = "https://simbe-dev.ulbi.ac.id/api/v1/webometrics/buku/citasiprodi";
