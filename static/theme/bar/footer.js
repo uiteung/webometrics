be1d90e52d52b414a3b36498fd6aac32bff74a28
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-15 14:31:38 +0700

    change class to id for render navbar & footer

--- static/theme/bar/footer.js
+++ static/theme/bar/footer.js
@@ -1,4 +1,4 @@
 // Untuk render footer
 document.addEventListener('DOMContentLoaded', function() {
-    $(".footer").load("https://euis.ulbi.ac.id/webometrics/static/theme/footer.html");
+    $("#footer").load("https://euis.ulbi.ac.id/webometrics/static/theme/footer.html");
 });
--- static/theme/bar/navbar.js
+++ static/theme/bar/navbar.js
@@ -1,4 +1,4 @@
 // Untuk render navbar
 document.addEventListener('DOMContentLoaded', function() {
-    $(".navbar").load("https://euis.ulbi.ac.id/webometrics/static/theme/navbar.html");
+    $("#navbar").load("https://euis.ulbi.ac.id/webometrics/static/theme/navbar.html");
 });
