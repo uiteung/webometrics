0dbd0828b92f5f5cdfa0028fa2dc276f592c7d9c
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-19 15:01:42 +0700

    coba add js in html langsung

--- static/theme/bar/sidebar.js
+++ static/theme/bar/sidebar.js
@@ -1,5 +1,4 @@
 // Untuk render navbar
 document.addEventListener('DOMContentLoaded', function() {
     $("#sidebar").load("https://euis.ulbi.ac.id/webometrics/static/theme/sidebar.html");
-    feather.replace();
 });
