cff510f62be339ec39f951583d17340ed015d4fa
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-19 14:58:40 +0700

    add function replace feather

--- static/theme/bar/sidebar.js
+++ static/theme/bar/sidebar.js
@@ -1,4 +1,5 @@
 // Untuk render navbar
 document.addEventListener('DOMContentLoaded', function() {
     $("#sidebar").load("https://euis.ulbi.ac.id/webometrics/static/theme/sidebar.html");
+    feather.replace();
 });
